ab28aa7d15b3ec23f060b12b40f1939a79cfe6ff
MrBeee <38375483+MrBeee@users.noreply.github.com>
2025-10-01 22:03:17 +0200

    Moving to Python 3.12 on QGIS 3.34.13

--- _RunVsCode.bat
+++ _RunVsCode.bat
@@ -2,8 +2,8 @@
 @REM See: https://www.tutorialspoint.com/batch_script/batch_script_quick_guide.htm 
 @REM See: https://stackoverflow.com/questions/22716069/how-to-open-a-new-shell-in-cmd-then-run-script-in-a-new-shell
 
-@REM start "C:\Program Files\QGIS 3.28.1\OSGeo4W.bat" start cmd.exe /k  _VsCodeScript
-start "" "C:\Program Files\QGIS 3.28.1\OSGeo4W.bat" start cmd.exe /k  _VsCodeScript
+@REM start "C:\Program Files\QGIS 3.34.13\OSGeo4W.bat" start cmd.exe /k  _VsCodeScript
+start "" "C:\Program Files\QGIS 3.34.13\OSGeo4W.bat" start cmd.exe /k  _VsCodeScript
 @REM start "" "C:\Users\Bart\AppData\Local\Programs\Microsoft VS Code\Code.exe"
 
 
--- _VsCodeScript.bat
+++ _VsCodeScript.bat
@@ -3,8 +3,8 @@
 @REM See: https://stackoverflow.com/questions/22716069/how-to-open-a-new-shell-in-cmd-then-run-script-in-a-new-shell
 @REM See: https://stackoverflow.com/questions/5909012/windows-batch-script-launch-program-and-exit-console
 
-@REM cmd /k "C:\Users\Bart\AppData\Local\Programs\Microsoft VS Code\Code.exe"
-start "" "C:\Users\Bart\AppData\Local\Programs\Microsoft VS Code\Code.exe"
+@REM cmd /k "C:\Users\BartD\AppData\Local\Programs\Microsoft VS Code\Code.exe"
+start "" "C:\Users\BartD\AppData\Local\Programs\Microsoft VS Code\Code.exe"
 
 @rem pause
 exit
